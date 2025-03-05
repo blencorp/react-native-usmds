d5f4f271bff9c274c3db69b87602bffc5fa92b27
Julius Oh <julius@blencorp.com>
2025-03-05 15:33:21 -0800

    chore: deploy docs to vercel

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,12 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-    mdxRs: true
-  },
-  // Add this to ensure proper build output
-  output: 'standalone'
 };
 
 export default withMDX(config);
