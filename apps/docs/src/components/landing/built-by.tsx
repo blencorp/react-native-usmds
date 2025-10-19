e95631615d3cf65f4c282367df087d8a921b4f75
Mike Endale <mike@blencorp.com>
2025-10-19 01:40:16 -0400

    hotfix: adjust font color on built by section (#174)
    * hotfix: adjust font color on built by section

--- apps/docs/src/components/landing/built-by.tsx
+++ apps/docs/src/components/landing/built-by.tsx
@@ -47,7 +47,7 @@ export default function BuiltBy() {
           )}
 
           {company.description && (
-            <div className=" text-lg">{company.description}</div>
+            <div className="text-neutral-950 text-lg">{company.description}</div>
           )}
         </div>
       ))}
